b2129a54f87538e8f55f076fd79c811a05b1e4af
LaMonte <lnunn6@emich.edu>
2024-02-17 12:24:00 -0500

    added homepage.dart file

--- backend/aas/poll/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'poll'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:org_id>/', views.OrgView, name='org_view'),
-]
