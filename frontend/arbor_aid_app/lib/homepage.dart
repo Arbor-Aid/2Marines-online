42f17e47df38f16cc2948e504072f844e732034e
Dasha S <dskalit@umich.edu>
2024-02-17 15:08:27 -0500

    sum wrong w the pic :(

--- frontend/arbor_aid_app/lib/homepage.dart
+++ frontend/arbor_aid_app/lib/homepage.dart
@@ -10,7 +10,31 @@ class _HomePage extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 189, 33, 160),
+        backgroundColor: Colors.grey,
+        leading: IconButton(
+          icon: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+            ),
+            child: Image.asset(
+              'assets/images/C:\Users\dasha\Desktop\2marines\arbor-aid\frontend\arbor_aid_app\Two_Marines_logo.svg',
+              // TO DO: sum wrong w this link
+              width: 65,
+              height: 65,
+            ),
+          ),
+          onPressed: () {
+            // Something happens if we press it?
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              Scaffold.of(context).openDrawer(); // Or other action
+            },
+          ),
+        ],
       ),
     );
   }
--- frontend/arbor_aid_app/lib/main.dart
+++ frontend/arbor_aid_app/lib/main.dart
@@ -2,5 +2,21 @@ import 'package:arbor_aid_app/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(HomePage());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Arbor Aid App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Directionality(
+        textDirection: TextDirection.ltr,
+        child: HomePage(),
+      ),
+    );
+  }
 }
